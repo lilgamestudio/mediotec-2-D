f65e42274a0085967682df67fc978dc32f510358
lilgamestudio <luizpcavalcanti@gmail.com>
2026-06-06 17:43:48 -0300

    Update contacts.js

--- retalhos/js/contacts.js
+++ retalhos/js/contacts.js
@@ -1,1 +1,37 @@
 
+async function renderContacts(containerId){
+
+    const response =
+        await fetch("../data/npcs.json");
+
+    const npcs =
+        await response.json();
+
+    const container =
+        document.getElementById(containerId);
+
+    container.innerHTML = "";
+
+    Object.entries(npcs).forEach(
+        ([id, npc]) => {
+
+        const div =
+            document.createElement("div");
+
+        if(id === "ana" || isUnlocked(id)){
+
+            div.innerHTML =
+                `✓ ${npc.name}`;
+
+        }else{
+
+            div.innerHTML =
+                `🔒 ${npc.name}`;
+
+        }
+
+        container.appendChild(div);
+
+    });
+
+}
